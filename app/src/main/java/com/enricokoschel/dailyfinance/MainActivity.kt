fc986509580e12c4686d86dc35b4cffe55232061
Enrico Koschel <enricokoschel02@gmail.com>
2022-07-11 16:31:41 +0200

    Implement saving application state

--- app/src/main/java/com/enricokoschel/dailyfinance/MainActivity.kt
+++ app/src/main/java/com/enricokoschel/dailyfinance/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.enricokoschel.dailyfinance
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.enricokoschel.dailyfinance.databinding.ActivityMainBinding
+import java.io.FileNotFoundException
 import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,8 @@ class MainActivity : AppCompatActivity() {
 	}
 
 	private lateinit var binding: ActivityMainBinding
+
+	private val saveFileName = "SAVED_MONEY"
 
 	private var totalMoney = 0
 
@@ -29,17 +33,17 @@ class MainActivity : AppCompatActivity() {
 	}
 
 	private fun setMoneyText() {
-		val totalMoneyFloat = totalMoney / 100f
+		val totalMoneyDouble = totalMoney / 100.0
 
 		val remainingDays = 12
-		val dailyMoneyFloat = totalMoneyFloat / remainingDays
+		val dailyMoneyDouble = totalMoneyDouble / remainingDays
 
 		val remainingDaysString =
 			resources.getQuantityString(R.plurals.remaining_days_text, remainingDays, remainingDays)
 
 		binding.txtMoneyDaily.text =
-			resources.getString(R.string.daily_money_text, dailyMoneyFloat, remainingDaysString)
-		binding.txtMoneyTotal.text = resources.getString(R.string.total_money_text, totalMoneyFloat)
+			resources.getString(R.string.daily_money_text, dailyMoneyDouble, remainingDaysString)
+		binding.txtMoneyTotal.text = resources.getString(R.string.total_money_text, totalMoneyDouble)
 	}
 
 	private fun commonButtonHandler(type: ButtonType) {
@@ -56,7 +60,34 @@ class MainActivity : AppCompatActivity() {
 			}
 		}
 
+		saveMoney()
 		setMoneyText()
+	}
+
+	private fun saveMoney() {
+		val array = ByteArray(4)
+		array[0] = totalMoney.toByte()
+		array[1] = (totalMoney ushr 8).toByte()
+		array[2] = (totalMoney ushr 16).toByte()
+		array[3] = (totalMoney ushr 24).toByte()
+
+		applicationContext.openFileOutput(saveFileName, Context.MODE_PRIVATE).write(array)
+	}
+
+	private fun restoreMoney() {
+		totalMoney = try {
+			val array = ByteArray(4)
+			applicationContext.openFileInput(saveFileName).read(array)
+
+			val b3 = (array[3].toUInt() shl 24) and (0xFFu shl 24)
+			val b2 = (array[2].toUInt() shl 16) and (0xFFu shl 16)
+			val b1 = (array[1].toUInt() shl 8) and (0xFFu shl 8)
+			val b0 = array[0].toUInt() and 0xFFu
+
+			(b3 or b2 or b1 or b0).toInt()
+		} catch (e: FileNotFoundException) {
+			0
+		}
 	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,6 +95,7 @@ class MainActivity : AppCompatActivity() {
 		binding = ActivityMainBinding.inflate(layoutInflater)
 		setContentView(binding.root)
 
+		restoreMoney()
 		setMoneyText()
 
 		binding.btnReset.setOnLongClickListener {
